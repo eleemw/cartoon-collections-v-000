859b5ad111b710858a752c39c7064c12defcbcbf
Erica Murdock-Waters <ericahpa@gmail.com>
2018-03-09 14:34:39 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -23,6 +23,7 @@ def find_the_cheese(food)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
   food.find { |f| cheese_types.include?(f) }
+## initial code to pass lab below refactored to rb: 25
 #    if food.include?(cheese_types[0])
 #      cheese_types[0]
 #    elsif food.include?(cheese_types[1])
